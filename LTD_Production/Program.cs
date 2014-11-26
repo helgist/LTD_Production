1127008a7be21fd236bb0243663000fb5d5cd861
helgist <helgi.gudmunds@gmail.com>
2014-11-26 11:45:22 +0100

    Have a nice evening

---
+++ LTD_Production/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LTD_Production
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Hello you crazy fockers!!!!
+                How are you doing today?
+
+            Are you having a great team Day or what?
+
+                Hey Liverpool vill win tonight :)
+        }
+    }
+}
